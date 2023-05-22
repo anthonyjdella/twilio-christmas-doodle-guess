9dc5f204b70a76aa3020003ace2d20e20af98f0f
Anthony_Dellavecchia <dev@anthonydellavecchia.com>
2023-05-21 22:52:07 -0700

    feat(completed): working app which shows a new gif each time the game is played

--- pictionary/app.py
+++ pictionary/app.py
@@ -3,82 +3,99 @@ from quick_draw import quick_draw
 from quick_draw import host_asset
 
 from flask import Flask, request, session
-from twilio.rest import Client
-from twilio.twiml.messaging_response import MessagingResponse
+from flask_session import Session
+from flask_cors import CORS, cross_origin
+from time import sleep
+import threading
 
 
 app = Flask(__name__)
 app.secret_key = 'super_secret_key'
+app.config['SESSION_TYPE'] = 'filesystem'
 
 asset_url = 'https://pictionary-9376-dev.twil.io/images/quickdraw.gif'
 drawing_name = None
-twiml = MessagingResponse()
 
 is_playing = False
 
+Session(app)
 
-with app.app_context():
-    drawing_name = quick_draw()
+
+def initialize_game():
+    session.clear()
+    session['playing'] = False
+    session['lives'] = 3
+
+
+def start_new_game():
+    initialize_game()
+    session['playing'] = True
+    session['drawing_name'] = quick_draw()
+
+    countdown_thread = threading.Thread(target=send_countdown)
+    countdown_thread.start()
+
+    host_asset_thread = threading.Thread(target=host_asset_with_callback)
+    host_asset_thread.start()
+
+
+def send_asset_callback():
+    send_outbound_text(
+        'Guess the Christmas gift Santa 🎅 is making!\n\nYou have 3 guesses remaining.',
+        asset_url
+    )
+
+
+def host_asset_with_callback():
     host_asset()
+    sleep(5)
+    send_asset_callback()
+
+
+def send_countdown():
+    send_outbound_text('Game is starting soon...')
+    sleep(2)
+
+    for i in range(5, 0, -1):
+        send_outbound_text(str(i))
+        sleep(2.5)
+
+
+def handle_bad_guess():
+    session['lives'] -= 1
+    if session['lives'] == 0:
+        send_outbound_text(
+            f"Sorry, you ran out of attempts! Game over. 💔\n\nThe correct answer was: {session['drawing_name']}"
+        )
+        initialize_game()
+    else:
+        send_outbound_text(
+            f"Try again ❌. You have {session['lives']} guesses remaining."
+        )
+
+
+def handle_good_guess():
+    send_outbound_text('Correct ✅! You guessed it!')
+    initialize_game()
 
 
 @app.route("/pictionary", methods=['POST'])
 def receive_inbound_text():
-    session['drawing_name'] = drawing_name
     user_input = request.values.get('Body', None).lower()
 
+    if 'playing' not in session:
+        initialize_game()
 
-    def handle_new_game():
-        session['playing'] = True
-        session['lives'] = 3
-        print(session['playing'])
-        send_outbound_text('Guess the Christmas gift Santa 🎅 is making!\n\nYou have 3 guesses remaining.', asset_url)
-
-
-    def handle_invalid_game():
-        send_outbound_text(f'To play a new game, send "GO"')
-
-
-    def check_answer(user_input):
-        if user_input in drawing_name:
-            return True
-        return False
-
-
-    def handle_bad_guess():
-        session['lives'] -= 1
-        if session['lives'] == 0:
-            handle_game_over()
-            send_outbound_text(f"Sorry, you ran out of attempts! Game over. 💔\n\nThe correct answer was: {drawing_name}")
-        else:
-            send_outbound_text(f"Try again ❌. You have {session['lives']} guesses remaining.")
-
-
-    def handle_good_guess():
-        handle_game_over()
-        send_outbound_text('Correct ✅! You guessed it!')
-
-
-    def handle_game_over():
-        session.clear()
-        # drawing_name = quick_draw()
-        # host_asset()
-
-
-    if not session.get('playing'):
-        print('inside')
+    if not session['playing']:
         if 'go' in user_input:
-            handle_new_game()
+            start_new_game()
         else:
-            handle_invalid_game()
+            send_outbound_text('To play a new game, send "GO"')
     else:
-        correct = check_answer(user_input)
-        if not correct:
-            handle_bad_guess()
-        else:
+        if user_input == session['drawing_name']:
             handle_good_guess()
-
-
+        else:
+            handle_bad_guess()
     return '200'
 
 
